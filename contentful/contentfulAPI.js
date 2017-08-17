7ffdb77c5be968e7c1deb3ebc1a8a32362db5c30
Murad Khan <muradkhan101@gmail.com>
2017-08-16 18:46:58 -0700

    Template syntax fixes and Contentful call Fixed the syntax errors in the Nunjucks template files and added the code for creating a single post from a Contentful call

--- contentful/contentfulAPI.js
+++ contentful/contentfulAPI.js
@@ -1,42 +1,119 @@
 const contentful = require('contentful');
 const config = require('./config.json');
+const markdown = require('markdown').markdown;
+
+const typeId = {
+    author: "1kUEViTN4EmGiEaaeC6ouY",
+    category: "5KMiN6YPvi42icqAUQMCQe",
+    comment: "comment",
+    post: "2wKn6yEnZewu2SCCkus4as",
+    user: "user"
+}
 
 var client = contentful.createClient({
     accessToken: config.accessToken,
     space: config.space
 });
 
-exports.getEntry = (id, cb) => {
-    client.getEntry(id)
-    .then((response) => cb(response))
-    .catch(console.error)
+const getPhotoFromAuthor = (author) => {
+    var photoObj = author.fields.profilePhoto.fields.file.url;
+    if (photoObj !== undefined) {
+        return photoObj;
+    } else return "insert blank photo";
 }
-exports.getAllEntries = (cb) => {
-    client.getEntries()
-    .then((response) => cb(response.items))
-    .catch(console.error)
+
+const getCategories = (categoryObject) => {
+    var categories = [];
+    if (categoryObject) {
+        for (var i = 0; i < categoryObject.length; i++) {
+            categories.push(categoryObject[0].fields.title);
+        }
+    } else return ["No categories"];
+    return categories;
 }
-exports.getLinkedItems = (typeID, linkOn, targetID, cb) => {
+
+const getAuthorInfo = (authorObj) => {
+    var author = {};
+    author.name = authorObj.fields.name;
+    author.biography = authorObj.fields.biography;
+    // author.socialLinks = getSocialLinks(authorObj.fields.socialLinks);
+    author.photo = getPhotoFromAuthor(authorObj);
+    return author;
+}
+
+exports.flattenLists = (contentType, field, id) => {
+    var flattened = {};
     client.getEntries({
-        content_type: typeID,
-        linking_field: linkOn,
-        target_entry_id: targetID
-    })
-    .then((response) => cb(response.items))
-    .catch(console.error)
+        content_type: contentType,
+        linking_field: field,
+        target_entry_id: id
+    }).then(function(data) {
+        for (var key in data) {
+            flattened[key] = data[key];
+        }
+    }).catch(console.log)
+    return flattened;
 }
-exports.getCategory = (typeID, category, cb) => {
-    client.getEntries({
-        content_type: typeID,
-        "sys.fields.category": category
-    })
-        .then((response) => cb(response.items))
-        .catch(console.error)
+
+exports.extractPostInfo = (post) => {
+    var postInfo = {};
+    postInfo.title = post.fields.title;
+    postInfo.slug = post.fields.slug;
+    postInfo.body = markdown.toHTML(post.fields.body);
+    postInfo.categories = getCategories(post.fields.category);
+    postInfo.author = getAuthorInfo(post.fields.author[0]); // Assumes posts only have one author
+    return postInfo;
 }
-exports.search = (queryTerm, cb) => {
-    client.getEntries({
-        query: queryTerm
-    })
-        .then((response) => cb(response.items))
-        .catch(console.error)
-}
+
+exports.client = client;
+
+//
+// exports.getEntry = async (id) => {
+//     try {
+//         return await client.getEntry(id);
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+// exports.getAllEntries = async () => {
+//     try {
+//         return await client.getEntries();
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+// exports.getLinkedItems = async (typeID, linkOn, targetID) => {
+//     try {
+//         return await client.getEntries({
+//             content_type: typeID,
+//             linking_field: linkOn,
+//             target_entry_id: targetID
+//         });
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+// exports.getCategory = async (typeID, category) => {
+//     try {
+//         return await client.getEntries({
+//             content_type: typeID,
+//             "sys.fields.category": category
+//         });
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+// exports.search = async (queryTerm) => {
+//     try {
+//         return await client.getEntries({
+//             query: queryTerm
+//         });
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const gulp = require('gulp');
+const rename = require('gulp-rename');
 const nunjucks = require('gulp-nunjucks-html');
-const markdown = require('markdown');
-const contentfulAPI = require('./contentful/contentfulAPI');
+const contentful = require('./contentful/contentfulAPI');
 
-    contentfulAPI.getEntry('A96usFSlY4G0W4kwAqswk', function(data){
+// Query for all posts
+contentful.client.getEntries({
+    content_type: "2wKn6yEnZewu2SCCkus4as",
+    include: 3
+}).then(function(data) {
+    for (let i = 0; i < data.items.length; i++) {
+        var post = {};
+        post = contentful.extractPostInfo(data.items[0]);
+        console.log(post.author);
         gulp.src('./templates/post.njk')
             .pipe(nunjucks({
-                searchPaths: ['templates'],
-                locals: data.fields
+                searchPaths: ['./templates'],
+                locals: post
             }))
-            .pipe(gulp.dest('./build'));
-    })
+            .on('error', console.log)
+            .pipe(gulp.dest('./build/'+post.slug+'.html'));
+    }
+})
+
+
+// contentfulAPI.getEntry('A96usFSlY4G0W4kwAqswk', function(data){
+//     // data.fields.body = markdown.toHTML(data.fields.body);
+//     console.log(data);
+//     gulp.src('./templates/post.njk')
+//         .pipe(nunjucks({
+//             searchPaths: ['templates'],
+//             locals: data.fields
+//         }))
+//         .pipe(gulp.dest('./build'));
+// })
