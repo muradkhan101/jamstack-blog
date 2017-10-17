2d7ac1239c19fc8f781a5ff9c7db8cbb71f33c8c
Murad Khan <muradkhan101@gmail.com>
2017-10-17 15:18:30 -0700

    Added React component and SQL files

---
+++ components/CategoryDisplay/MainCategoryContainer.js
@@ -0,0 +1,1 @@
+const React=require('react');const react_redux=require('react-redux');const CategoriesContainer=require('./CategoriesContainer');const mapStateToProps=state=>{return{categories:state.categories,posts:state.posts}};let MainCategoryContainer=react_redux.connect(mapStateToProps)(CategoriesContainer);module.exports=MainCategoryContainer;
